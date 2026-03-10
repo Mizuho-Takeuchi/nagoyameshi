ce308297412038246885836fe2fe930ddf3c61c8
Mizuho-Takeuchi <wendy.mizuho@gmail.com>
2026-03-10 21:44:33 +0900

    feature-category-restaurant

--- src/main/java/com/example/nagoyameshi/form/CategoryRegisterForm.java
+++ src/main/java/com/example/nagoyameshi/form/CategoryRegisterForm.java
@@ -1,5 +1,7 @@
 package com.example.nagoyameshi.form;
 
+import java.util.List;
+
 import jakarta.validation.constraints.NotBlank;
 
 import lombok.Data;
@@ -8,4 +10,6 @@ import lombok.Data;
 public class CategoryRegisterForm {
 	@NotBlank(message = "カテゴリを入力してください。")
 	private String name;
+	
+	private List<Integer> categoryIds;
 }
